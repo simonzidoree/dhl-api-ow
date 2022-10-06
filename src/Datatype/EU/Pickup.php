23899433cc4b161f939fac2c782d957ab171c44f
Simon Zidoree <simzo96@hotmail.com>
2022-10-06 13:52:15 +0200

    Added PickupTypeCode to Pickup datatype

--- src/Datatype/EU/Pickup.php
+++ src/Datatype/EU/Pickup.php
@@ -16,23 +16,31 @@ class Pickup extends Base
      * @var array
      */
     protected $params = [
-        'PickupDate'  => [
-            'type'      => 'string',
-            'required'  => true,
+        'PickupDate' => [
+            'type' => 'string',
+            'required' => true,
             'subobject' => false,
-            'comment'   => 'Date Y-m-d',
+            'comment' => 'Date Y-m-d',
             'maxLength' => '10',
         ],
-        'ReadyByTime' => [
-            'type'      => 'string',
-            'required'  => true,
+        'PickupTypeCode' => [
+            'type' => 'PickupTypeCode',
+            'required' => true,
             'subobject' => false,
-            'comment'   => 'Time HH:MM',
+            'comment' => 'Not sure',
+            'length' => '1',
+            'enumeration' => 'S,A',
+        ],
+        'ReadyByTime' => [
+            'type' => 'string',
+            'required' => true,
+            'subobject' => false,
+            'comment' => 'Time HH:MM',
             'maxLength' => '5',
         ],
-        'CloseTime'   => [
-            'type'      => 'string',
-            'required'  => true,
+        'CloseTime' => [
+            'type' => 'string',
+            'required' => true,
             'maxLength' => '5',
             'comment'   => 'Time HH:MM',
             'subobject' => false,
